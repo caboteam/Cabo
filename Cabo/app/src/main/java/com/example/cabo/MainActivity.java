7e8d85a141783c6eb77a7b55a7efbbaec555477e
SirHappi <sri13.vinukonda@gmail.com>
2019-11-20 17:58:36 -0500

    Added Background animations

--- Cabo/app/src/main/java/com/example/cabo/MainActivity.java
+++ Cabo/app/src/main/java/com/example/cabo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.cabo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -27,5 +29,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout background = (ConstraintLayout) findViewById(R.id.background);
+        AnimationDrawable animation = (AnimationDrawable) background.getBackground();
+
+        animation.setEnterFadeDuration(3000);
+        animation.setExitFadeDuration(3000);
+        animation.start();
+
     }
 }
