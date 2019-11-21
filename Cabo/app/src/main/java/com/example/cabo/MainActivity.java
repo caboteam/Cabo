4a64fc19893b22cbede93d37850b816f48001559
SirHappi <sri13.vinukonda@gmail.com>
2019-11-20 22:09:23 -0500

    Updated buttons.
    Chnaged opacity and what not.

--- Cabo/app/src/main/java/com/example/cabo/MainActivity.java
+++ Cabo/app/src/main/java/com/example/cabo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cabo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
@@ -33,9 +34,16 @@ public class MainActivity extends AppCompatActivity {
         ConstraintLayout background = (ConstraintLayout) findViewById(R.id.background);
         AnimationDrawable animation = (AnimationDrawable) background.getBackground();
 
-        animation.setEnterFadeDuration(3000);
+        animation.setEnterFadeDuration(0);
         animation.setExitFadeDuration(3000);
+
         animation.start();
+
+//        AnimationDrawable butt_anim = (AnimationDrawable) findViewById(R.id.button2).getBackground();
+//        butt_anim.setEnterFadeDuration(3000);
+//        butt_anim.setExitFadeDuration(6000);
+//        butt_anim.start();Q
+
 
     }
 }
